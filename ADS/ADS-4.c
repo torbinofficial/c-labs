49b7561b605fc5602f05d0397f37ac03bb0e80aa
torbinofficial <torbin.official@gmail.com>
2018-12-03 14:23:15 +0200

    And some more structures

--- ADS/ADS-4.c
+++ ADS/ADS-4.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 typedef struct list{
-  char title[10] ;
+  char *title ;
   int amount_of_students;
   struct list * next;
 } linked_list;
@@ -9,12 +9,14 @@ main()
 {
   linked_list group;
   int N;
+  char *ptr;
+  ptr = (char *)malloc(6);
   printf("Please, enter amount or groups: \n");
   scanf("%d", &N);
   for (int i = 0;i < N; i++)
   {
     printf("Enter name of group:\n");
-    fgets(group.title, 11, stdin);
+    //gets(temp);
     printf("Enter amount of students:\n");
     scanf("%d", &group.amount_of_students);
    }
