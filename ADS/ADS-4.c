0b6da235472c8119ece0cb3f255c534a9f611f89
torbinofficial <torbin.official@gmail.com>
2018-12-03 18:18:14 +0200

    Still working

--- ADS/ADS-4.c
+++ ADS/ADS-4.c
@@ -1,26 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
-typedef struct list{
-  char *title ;
+typedef struct group_name
+{
   int amount_of_students;
-  struct list * next;
+  char * title;
+}group;
+
+typedef struct list
+{
+  group gn;
+  struct list *next;
 } linked_list;
 main()
 {
-  linked_list group;
+  linked_list *l_l;
   int N;
-  group.title = (char *)malloc(6);
-  if (group.title)
-  {
-    printf("Hoorray!");
-  }
   printf("Please, enter amount or groups: \n");
   scanf("%d", &N);
-  for (int i = 0;i < N; i++)
+  l_l=calloc(N ,sizeof(linked_list));
+  l_l /*-> head*/ -> next = NULL;
+  for (int i = 0; i < N; i++)
   {
+    l_l->gn.title = (char *)malloc(6);
     printf("Enter name of group:\n");
-    //gets(temp);
+    scanf("%s", l_l->gn.title);
     printf("Enter amount of students:\n");
-    scanf("%d", &group.amount_of_students);
+    scanf("%d", &l_l->gn.amount_of_students);
    }
+   printf("%s\n", l_l->gn.title);
+   printf("%d", l_l->gn.amount_of_students);
 }
