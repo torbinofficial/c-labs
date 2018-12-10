867357a51c657d947d1046b05512aba325354ad0
torbinofficial <torbin.official@gmail.com>
2018-12-10 22:47:00 +0200

    Linked list

--- ADS/ADS-4.c
+++ ADS/ADS-4.c
@@ -1,32 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
-typedef struct group_name
-{
-  int amount_of_students;
-  char * title;
-}group;
-
 typedef struct list
 {
-  group gn;
+  int amount_of_students;
+  char *title;
   struct list *next;
 } linked_list;
-main()
+linked_list *create_node(char name, int amount);
+ main()
 {
-  linked_list *l_l;
-  int N;
-  printf("Please, enter amount or groups: \n");
-  scanf("%d", &N);
-  l_l=calloc(N ,sizeof(linked_list));
-  l_l /*-> head*/ -> next = NULL;
-  for (int i = 0; i < N; i++)
+  int N, i;
+  linked_list *ll, pw;
+  linked_list *head = malloc(sizeof(linked_list));
+  printf("Enter amount of groups\n");
+  scanf("%d\n", &N);
+  ll = calloc(N, sizeof(linked_list));
+  head = create_node('0', 0);
+  for (i = 1, ll = head; i < N;)
   {
-    l_l->gn.title = (char *)malloc(6);
-    printf("Enter name of group:\n");
-    scanf("%s", l_l->gn.title);
-    printf("Enter amount of students:\n");
-    scanf("%d", &l_l->gn.amount_of_students);
-   }
-   printf("%s\n", l_l->gn.title);
-   printf("%d", l_l->gn.amount_of_students);
+    ll->title = (char*)malloc(6);
+    printf("Enter name of group\n");
+    fgets(ll->title, 6, stdin);
+    printf("Enter amount of students\n");
+    scanf("%d\n", &ll->amount_of_students);
+    ll=ll->next;
+  }
+  linked_list *create_node(char name, int amount)
+  {
+   linked_list *result;
+   result = (linked_list*)malloc(sizeof(linked_list));
+   printf("Enter name of group\n");
+   scanf("%s\n", result->title);
+   printf("Enter amount of students\n");
+   scanf("%d", &result->amount_of_students);
+   result->next=NULL;
+   return result;
+  }
 }
